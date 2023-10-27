4d5741649dcb8651e1a02732b6bd1eaa50fdda36
julia prado <108271775+jul1a150@users.noreply.github.com>
2023-10-27 16:30:53 -0300

    f43frf

--- main.js
+++ main.js
@@ -4,3 +4,34 @@ function tocaSomPom () {
 }
 
 document.querySelector('.tecla_pom').onclick=tocaSomPom;
+
+function tocaSomPuff (){
+    document.querySelector('#som_tecla_puff').play();
+
+}
+
+document.querySelector('.tecla_puff').onclick=tocaSomPuff;
+
+function tocaSomClap (){
+    document.querySelector('#som_tecla_clap').play();
+
+}
+document.querySelector('.tecla_clap').onclick=tocaSomClap;
+
+function tocaSomTim(){
+    document .querySelector( '#som_tecla_tim').play;
+
+}
+document.querySelector('.tecla_tim').onclick=tocaSomTim;
+
+function tocaSomSplash(){
+    document .querySelector( '#som_tecla_splash').play;
+
+}
+document.querySelector('.tecla_splash').onclick=tocaSomSplash;
+
+function tocaSomToim(){
+    document .querySelector( '#som_tecla_toim').play;
+
+}
+document.querySelector('.tecla_toim').onclick=tocaSomToim;
